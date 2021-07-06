ad42bf4217a03cb6f57855e2532adf4169a7a5a2
TylinnLaShay <tylinnhall@gmail.com>
2021-07-06 16:03:06 -0700

    user friendly improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,14 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function() {
-
+  
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
     //package data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -13,6 +17,7 @@ var taskFormHandler = function() {
     };
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
+    formEl.reset();
 }
 var createTaskEl = function(taskDataObj) {
     //create list item
